98faee709dfed6d5e3611082765c05c080ce242c
Ryan Wessel <ryan.wessel@student.fairfield.edu>
2015-04-30 12:20:13 -0400

    added scroll nav effect

--- js/main.js
+++ js/main.js
@@ -2,9 +2,23 @@ $(document).ready (function () {
  $(window).scroll (function () {
     var sT = $(this).scrollTop();
         if (sT >= 20) {
-            $('.butImNotAWrapper').addClass('cambio')
+            $('.butImNotAWrapper').addClass('mask')
         }else {
-            $('.butImNotAWrapper').removeClass('cambio')
+            $('.butImNotAWrapper').removeClass('mask')
         }
   })
 })
+
+$(window).scroll(
+    {
+        previousTop: 0
+    }, 
+    function () {
+    var currentTop = $(window).scrollTop();
+    if (currentTop < this.previousTop) {
+        $(".navbar-default").show();
+    } else {
+        $(".navbar-default").hide();
+    }
+    this.previousTop = currentTop;
+});
